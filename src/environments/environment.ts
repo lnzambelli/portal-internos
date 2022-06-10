27499804a535eb5907c0108c2b65085a727ad130
Luis Zambelli <69328669+lnzambelli@users.noreply.github.com>
2022-06-10 10:22:05 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyBc5-S4pA9hQIPFbpGN5ZHeLcbWP7snIQ8",
-    authDomain: "portal-internos.firebaseapp.com",
-    databaseURL: "https://portal-internos-default-rtdb.firebaseio.com",
-    projectId: "portal-internos",
-    storageBucket: "portal-internos.appspot.com",
-    messagingSenderId: "205371250410",
-    appId: "1:205371250410:web:4beb0d193cfde62d59e993"
+    
   }
 };
